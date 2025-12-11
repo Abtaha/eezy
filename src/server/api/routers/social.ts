f5af8f599b67103fbaef150ac1b8b8b7ecc2b431
Abtaha Farooq <abtaha2005@gmail.com>
2025-12-11 17:57:15 +0300

    Add router to trpc root

--- src/server/api/routers/social.ts
+++ src/server/api/routers/social.ts
@@ -1,103 +1,124 @@
-import {
-  createTRPCRouter,
-  protectedProcedure,
-} from "@/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 import { z } from "zod";
 import { db } from "@/server/db";
-import { product, ratings, comments, orders, orderItems } from "@/server/db/schema";
-import { eq, and, } from "drizzle-orm";
+import {
+  product,
+  ratings,
+  comments,
+  orders,
+  orderItems,
+} from "@/server/db/schema";
+import { eq, and } from "drizzle-orm";
 import { TRPCError } from "@trpc/server";
 
 //helper function that checks if user has the product delivered
-async function productDeliveredToUser(userID: string, productID: string){
+async function productDeliveredToUser(userID: string, productID: string) {
+  //Check if product exists
+  const productExists = await db
+    .select()
+    .from(product)
+    .where(eq(product.id, productID));
 
-    //Check if product exists
-    const productExists = await db
-        .select()
-        .from(product)
-        .where(eq(product.id, productID))
-           
-    if (productExists.length == 0){
-        throw new TRPCError({
-            code: "NOT_FOUND",
-            message: `Product ${productID} not found`,
-        });
-    }
+  if (productExists.length == 0) {
+    throw new TRPCError({
+      code: "NOT_FOUND",
+      message: `Product ${productID} not found`,
+    });
+  }
 
-    //get delivered products of the user
-    const deliveredProducts = await db
-        .select()
-        .from(orders)
-        .innerJoin(orderItems, eq(orders.id, orderItems.orderId)) //check corresponding order items
-        .where(
-            and(
-                eq(orders.userId, userID),
-                eq(orders.status, "delivered"),
-                eq(orderItems.productId, productID)
-            )
-        );
-    return deliveredProducts.length > 0;
+  //get delivered products of the user
+  const deliveredProducts = await db
+    .select()
+    .from(orders)
+    .innerJoin(orderItems, eq(orders.id, orderItems.orderId)) //check corresponding order items
+    .where(
+      and(
+        eq(orders.userId, userID),
+        eq(orders.status, "delivered"),
+        eq(orderItems.productId, productID),
+      ),
+    );
+  return deliveredProducts.length > 0;
 }
 
 export const socialRouter = createTRPCRouter({
-    addRating: protectedProcedure
-        .input(z.object({
-            productId: z.string().uuid(),
-            rating: z.number().min(1).max(5), // Rating between 1 and 5
-        }))
-        .mutation(async ({ ctx, input }) => {
-            const userId = ctx.session.user.id;
-            
-            const mayRate = await productDeliveredToUser(userId, input.productId);
+  canRate: protectedProcedure
+    .input(
+      z.object({
+        productId: z.string().uuid(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const userId = ctx.session.user.id;
 
-            if (!mayRate) {
-                throw new TRPCError({
-                    code: "FORBIDDEN",
-                    message: `User has not received product ${input.productId}.`,
-                });
-            }
+      const mayRate = await productDeliveredToUser(userId, input.productId);
 
-            //insert rating into db
-            const newRating = await db
-                .insert(ratings)
-                .values({
-                    userId: userId,
-                    productId: input.productId,
-                    rating: input.rating,
-                })
-                .returning();
-            
-            return newRating[0];
-        }),
-    
-    addComment: protectedProcedure
-        .input(z.object({
-            productId: z.string().uuid(),
-            comment: z.string().min(1).max(1000), // Comment text between 1 and 1000 characters
-        }))
-        .mutation(async ({ ctx, input }) => {
-            const userId = ctx.session.user.id;
+      return mayRate;
+    }),
 
-            const mayComment = await productDeliveredToUser(userId, input.productId);
+  addRating: protectedProcedure
+    .input(
+      z.object({
+        productId: z.string().uuid(),
+        rating: z.number().min(1).max(5), // Rating between 1 and 5
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const userId = ctx.session.user.id;
 
-            if (!mayComment) {
-                throw new TRPCError({
-                    code: "FORBIDDEN",
-                    message: `User has not received product ${input.productId}.`,
-                });
-            }
+      const mayRate = await productDeliveredToUser(userId, input.productId);
 
-            //insert comment into db
-            const newComment = await db
-                .insert(comments)
-                .values({
-                    userId: userId,
-                    productId: input.productId,
-                    comment: input.comment,
-                    approved: false, //Comment needs approval by default
-                })
-                .returning();
+      if (!mayRate) {
+        throw new TRPCError({
+          code: "FORBIDDEN",
+          message: `User has not received product ${input.productId}.`,
+        });
+      }
 
-            return newComment[0];
-        }),
+      //insert rating into db
+      const newRating = await db
+        .insert(ratings)
+        .values({
+          userId: userId,
+          productId: input.productId,
+          rating: input.rating,
+        })
+        .returning();
+
+      return newRating[0];
+    }),
+
+  addComment: protectedProcedure
+    .input(
+      z.object({
+        productId: z.string().uuid(),
+        comment: z.string().min(1).max(1000), // Comment text between 1 and 1000 characters
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const userId = ctx.session.user.id;
+
+      const mayComment = await productDeliveredToUser(userId, input.productId);
+
+      if (!mayComment) {
+        throw new TRPCError({
+          code: "FORBIDDEN",
+          message: `User has not received product ${input.productId}.`,
+        });
+      }
+
+      //insert comment into db
+      const newComment = await db
+        .insert(comments)
+        .values({
+          userId: userId,
+          productId: input.productId,
+          comment: input.comment,
+          approved: false, //Comment needs approval by default
+        })
+        .returning();
+
+      return newComment[0];
+    }),
 });
+
