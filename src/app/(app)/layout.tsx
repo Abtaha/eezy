044c10c018a6631c3aae6030b0d06e900f7c651e
Abtaha Farooq <abtaha2005@gmail.com>
2026-01-13 05:55:34 +0300

    add uploadthing next plugin

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,7 +3,6 @@ import "@/styles/globals.css";
 import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 
-import { TRPCReactProvider } from "@/trpc/react";
 import { Header } from "@/components/layout-components/header";
 import { Footer } from "@/components/layout-components/footer";
 import { CartProvider } from "@/context/cart-context";
